f06d5c763b6139add6324eec949bfc05e25d36b2
Giorgi Kantsliani <gqancliani@gmail.com>
2024-02-07 18:17:08 +0400

    chore: added env variables for database

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    mongodb_username: "gqancliani",
+    mongodb_password: "8vmi5jtoCluoGJpw",
+    mongodb_database: "my-blog-app",
+    mongodb_clustername: "kantslo-next-blog",
+  },
+};
