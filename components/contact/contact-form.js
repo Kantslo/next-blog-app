a1d39125ede429eb9d65e70bfe5a29e91b644c46
Giorgi Kantsliani <gqancliani@gmail.com>
2024-02-07 17:11:06 +0400

    chore: small syntax changes

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import classes from "./contact-form.module.css"
+import classes from "./contact-form.module.css";
 import Notification from "../ui/notification";
 
 const sendContactData = async (contactDetails) => {
@@ -9,15 +9,15 @@ const sendContactData = async (contactDetails) => {
     body: JSON.stringify(contactDetails),
     headers: {
       "Content-Type": "application/json",
-    }
+    },
   });
 
   const data = await response.json();
 
   if (!response.ok) {
-    throw new Error(data.message) || "Something went wrong!"
+    throw new Error(data.message) || "Something went wrong!";
   }
-}
+};
 
 function ContactForm() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -31,34 +31,34 @@ function ContactForm() {
       const timer = setTimeout(() => {
         setRequestStatus(null);
         setRequestError(null);
-      }, 3000)
+      }, 3000);
 
       return () => clearTimeout(timer);
     }
-  }, [requestStatus])
+  }, [requestStatus]);
 
   const sendMessageHandler = async (e) => {
     e.preventDefault();
 
     // ? Here could be client side validation for the form
 
-    setRequestStatus("pending")
+    setRequestStatus("pending");
 
     try {
       await sendContactData({
         email: enteredEmail,
         name: enteredName,
-        message: enteredMessage
+        message: enteredMessage,
       });
-      setRequestStatus("success")
-      setEnteredEmail("")
-      setEnteredName("")
-      setEnteredMessage("")
+      setRequestStatus("success");
+      setEnteredEmail("");
+      setEnteredName("");
+      setEnteredMessage("");
     } catch (error) {
-      setRequestError(error.message)
-      setRequestStatus("error")
+      setRequestError(error.message);
+      setRequestStatus("error");
     }
-  }
+  };
 
   let notification;
 
@@ -66,7 +66,7 @@ function ContactForm() {
     notification = {
       status: "pending",
       title: "Sending message...",
-      message: "Your message is on its way!"
+      message: "Your message is on its way!",
     };
   }
 
@@ -74,7 +74,7 @@ function ContactForm() {
     notification = {
       status: "success",
       title: "Success",
-      message: "Message sent successfully!"
+      message: "Message sent successfully!",
     };
   }
 
@@ -82,33 +82,58 @@ function ContactForm() {
     notification = {
       status: "error",
       title: "Error",
-      message: requestError
+      message: requestError,
     };
   }
 
-  return <section className={classes.contact}>
-    <h1>How can I help you?</h1>
-    <form className={classes.form} onSubmit={sendMessageHandler}>
-      <div className={classes.controls}>
-        <div className={classes.control}>
-          <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required value={enteredEmail} onChange={(e) => setEnteredEmail(e.target.value)} />
+  return (
+    <section className={classes.contact}>
+      <h1>How can I help you?</h1>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
+        <div className={classes.controls}>
+          <div className={classes.control}>
+            <label htmlFor="email">Your Email</label>
+            <input
+              type="email"
+              id="email"
+              required
+              value={enteredEmail}
+              onChange={(e) => setEnteredEmail(e.target.value)}
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="name">Your Email</label>
+            <input
+              type="text"
+              id="name"
+              required
+              value={enteredName}
+              onChange={(e) => setEnteredName(e.target.value)}
+            />
+          </div>
         </div>
         <div className={classes.control}>
-          <label htmlFor="name">Your Email</label>
-          <input type="text" id="name" required value={enteredName} onChange={(e) => setEnteredName(e.target.value)} />
+          <label htmlFor="message">Your Message</label>
+          <textarea
+            id="message"
+            rows="5"
+            required
+            value={enteredMessage}
+            onChange={(e) => setEnteredMessage(e.target.value)}></textarea>
         </div>
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="message">Your Message</label>
-        <textarea id="message" rows="5" required value={enteredMessage} onChange={(e) => setEnteredMessage(e.target.value)}></textarea>
-      </div>
-      <div className={classes.actions}>
-        <button>Send Message</button>
-      </div>
-    </form>
-    {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
-  </section>;
+        <div className={classes.actions}>
+          <button>Send Message</button>
+        </div>
+      </form>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+    </section>
+  );
 }
 
 export default ContactForm;
