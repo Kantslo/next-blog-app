f3752f0a0b2ec3de7da2053fe4e3bd261a76377b
Giorgi Kantsliani <gqancliani@gmail.com>
2024-02-08 21:57:00 +0400

    chore: created local variables

--- next.config.js
+++ next.config.js
@@ -4,20 +4,20 @@ module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       env: {
-        mongodb_username: process.env.MONGODB_USERNAME,
-        mongodb_password: process.env.MONGODB_PASSWORD,
-        mongodb_clustername: process.env.MONGODB_CLUSTERNAME,
-        mongodb_database: process.env.MONGODB_DATABASE_DEV,
+        mongodb_username: "gqancliani",
+        mongodb_password: "8vmi5jtoCluoGJpw",
+        mongodb_clustername: "kantslo-next-blog",
+        mongodb_database: "my-blog-app-dev",
       },
     };
   }
 
   return {
     env: {
-      mongodb_username: process.env.MONGODB_USERNAME,
-      mongodb_password: process.env.MONGODB_PASSWORD,
-      mongodb_clustername: process.env.MONGODB_CLUSTERNAME,
-      mongodb_database: process.env.MONGODB_DATABASE,
+      mongodb_username: "gqancliani",
+      mongodb_password: "8vmi5jtoCluoGJpw",
+      mongodb_clustername: "kantslo-next-blog",
+      mongodb_database: "my-blog-app",
     },
   };
 };
