24c6b541d6fc8a13f928f012814c161eda0f52f2
Giorgi Kantsliani <gqancliani@gmail.com>
2024-02-08 21:39:30 +0400

    chore: created env variables

--- next.config.js
+++ next.config.js
@@ -1,23 +1,24 @@
+require("dotenv").config();
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       env: {
-        mongodb_username: "gqancliani",
-        mongodb_password: "8vmi5jtoCluoGJpw",
-        mongodb_clustername: "kantslo-next-blog",
-        mongodb_database: "my-blog-app-dev",
+        mongodb_username: process.env.MONGODB_USERNAME,
+        mongodb_password: process.env.MONGODB_PASSWORD,
+        mongodb_clustername: process.env.MONGODB_CLUSTERNAME,
+        mongodb_database: process.env.MONGODB_DATABASE_DEV,
       },
     };
   }
 
   return {
     env: {
-      mongodb_username: "gqancliani",
-      mongodb_password: "8vmi5jtoCluoGJpw",
-      mongodb_clustername: "kantslo-next-blog",
-      mongodb_database: "my-blog-app",
+      mongodb_username: process.env.MONGODB_USERNAME,
+      mongodb_password: process.env.MONGODB_PASSWORD,
+      mongodb_clustername: process.env.MONGODB_CLUSTERNAME,
+      mongodb_database: process.env.MONGODB_DATABASE,
     },
   };
 };
