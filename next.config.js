48aa0db55118a96eb4ae1e610002e1d2e21a1425
Giorgi Kantsliani <gqancliani@gmail.com>
2024-02-08 21:12:28 +0400

    chore: optimized prism and syntaxhighlighter for post content page

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
-module.exports = {
-  env: {
-    mongodb_username: "gqancliani",
-    mongodb_password: "8vmi5jtoCluoGJpw",
-    mongodb_database: "my-blog-app",
-    mongodb_clustername: "kantslo-next-blog",
-  },
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: "gqancliani",
+        mongodb_password: "8vmi5jtoCluoGJpw",
+        mongodb_clustername: "kantslo-next-blog",
+        mongodb_database: "my-blog-app-dev",
+      },
+    };
+  }
+
+  return {
+    env: {
+      mongodb_username: "gqancliani",
+      mongodb_password: "8vmi5jtoCluoGJpw",
+      mongodb_clustername: "kantslo-next-blog",
+      mongodb_database: "my-blog-app",
+    },
+  };
 };
